20c6bae2c7e47eaeb16030ff796f9288730f99c0
kcru0804 <kcruz080492@gmail.com>
2025-07-18 14:33:12 -0600

    Agregando media queries

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,23 +3,27 @@ import { useState } from 'react'
 import '../styles/Navbar.css'
 
 const Navbar = () => {
-  const [menuAbierto, setMenuAbierto] = useState(false)
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
 
   const toggleMenu = () => {
-    setMenuAbierto(!menuAbierto)
+    setIsMenuOpen(!isMenuOpen)
   }
 
   return (
     <nav className="navbar">
       <h1>Kodigo Music</h1>
 
-      <button className="hamburger" onClick={toggleMenu}>
-        ☰
+      {/* Botón hamburguesa */}
+      <button className="hamburger" onClick={toggleMenu} aria-label="Toggle menu">
+        <span className={`bar ${isMenuOpen ? 'animate' : ''}`}></span>
+        <span className={`bar ${isMenuOpen ? 'animate' : ''}`}></span>
+        <span className={`bar ${isMenuOpen ? 'animate' : ''}`}></span>
       </button>
 
-      <ul className={menuAbierto ? 'menu abierto' : 'menu'}>
-        <li><Link to="/" onClick={() => setMenuAbierto(false)}>Inicio</Link></li>
-        <li><Link to="/login" onClick={() => setMenuAbierto(false)}>Login</Link></li>
+      {/* Menú de navegación */}
+      <ul className={`nav-links ${isMenuOpen ? 'open' : ''}`}>
+        <li><Link to="/" onClick={() => setIsMenuOpen(false)}>Inicio</Link></li>
+        <li><Link to="/login" onClick={() => setIsMenuOpen(false)}>Login</Link></li>
       </ul>
     </nav>
   )
